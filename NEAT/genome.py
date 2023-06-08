22dca2e9e424437d90fe297c6ef05cecc998ef97
Vicente Cser <vicentecser@gmail.com>
2023-06-08 11:56:50 -0400

    Fix bug in Genome._distance

--- NEAT/genome.py
+++ NEAT/genome.py
@@ -3,9 +3,6 @@ from connection_gene import ConnectionGene
 from node_gene import NodeGene
 from typing import Self
 
-# links need to be added significantly more often than nodes,
-# and an average weight difference of 3.0 is about as
-# significant as one disjoint or excess gene.
 
 class Genome:
     def __init__(self, neat: NEAT):
@@ -61,34 +58,34 @@ class Genome:
                 disjoint_genes += 1
                 innovation1 += 1
 
-            weight_difference /= similar_genes
+        weight_difference /= similar_genes
+        excess_genes = len(genome1.connections) - genome1_index
+        
+        # N can be set to 1 if both genomes are small, i.e., consist of fewer than 20 gene
+        N: float = max(len(genome1.connections), len(genome2.connections))
+        N = 1 if N < 20 else N
 
-            excess_genes = len(genome1.connections) - genome1_index
-
-            N: float = max(len(genome1.connections), len(genome2.connections))
-            N = 1 if N < 20 else N
-
-            return self.neat.C1 * (disjoint_genes/N) + self.neat.C2 * (excess_genes/N) + self.neat.C3 * weight_difference
+        return self.neat.C1 * (disjoint_genes/N) + self.neat.C2 * (excess_genes/N) + self.neat.C3 * weight_difference
 
     def _cross_over(self, genome1, genome2):
         pass
 
-    # There was a 75% chance that an inherited gene was disabled if it was disabled in either parent. 
+    # There was a 75% chance that an inherited gene was disabled if it was disabled in either parent.
     # In each generation, 25% of offspring resulted from mutation without crossover
     def _mutate(self):
         pass
 
+    # a single new connection gene with a random weight is added connecting two previously unconnected nodes
     def _mutate_link(self):
-        # a single new connection gene with a random weight is added connecting two previously unconnected nodes
         pass
 
+    # an existing connection is split and the new node placed where the old connection used to be
+    # The old connection is disabled and two new connections are added to the genome
+    # The new connection leading into the new node receives a weight of 1, and the new connection
+    # leading out receives the same weight as the old connection
     def _mutate_node(self):
-        # an existing connection is split and the new node placed where the old connection used to be
-        # The old connection is disabled and two new connections are added to the genome
-        # The new connection leading into the new node receives a weight of 1, and the new connection 
-        # leading out receives the same weight as the old connection
         pass
-    
+
     # There was an 80% chance of a genome having its connection weights mutated,
     # in which case each weight had a 90% chance of being uniformly perturbed
     # and a 10% chance of being assigned a new random value.
@@ -97,8 +94,8 @@ class Genome:
 
     def _mutate_weight_random(self):
         pass
-    
-    # There was a 75% chance that an inherited gene was disabled if it was disabled 
+
+    # There was a 75% chance that an inherited gene was disabled if it was disabled
     # in either parent. In each generation, 25% of offspring resulted from mutation without crossover
     def _mutate_link_toggle(self):
         pass
