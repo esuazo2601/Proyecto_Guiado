a99c8bd0acd33a353625d52811cf6590d9c6123e
sebarobot <sebastiancordova11@gmail.com>
2023-07-05 21:47:18 -0400

    Just comments
    :)

--- NEAT/neural_network.py
+++ NEAT/neural_network.py
@@ -1,5 +1,12 @@
+from .neat import NEAT
 # Clase encargada de ejecutar la red seleccionada por neat y reconstruirla
 # a partir de los archivos .pickle generados por este
 class NeuralNetwork:
     def __init__(self):
+        pass
+
+    def train(self):        # Entrenar el modelo
+        pass
+
+    def test(self):         # Probar un modelo ya entrenado
         pass
--- NEAT/node_gene.py
+++ NEAT/node_gene.py
@@ -2,5 +2,5 @@ from gene import Gene
 
 class NodeGene(Gene):
     def __init__(self, id: int, type: str):
-        self.id = id
-        self.type = type
+        self.id = id        # Numero de nodo
+        self.type = type    # Tipo de nodo (INPUT, OUTPUT, HIDDEN)
