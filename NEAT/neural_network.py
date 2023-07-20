421c7c5524e0d967d4621cdab42d23a1aec283c2
Vicente Cser <vicentecser@gmail.com>
2023-07-20 09:05:24 -0400

    Implement feed forward

--- NEAT/neural_network.py
+++ NEAT/neural_network.py
@@ -10,8 +10,9 @@ from typing import Self
 class Node:
     def __init__(self, type: str) -> None:
         self.input: list[Self] = []
-        self.output: float
-        self.weight: dict[float]
+        self.output = list[Self] = []
+        self.value: float = 0
+        self.weight: dict[Self]
         self.type = type
         self.ready = False
 
@@ -19,6 +20,7 @@ class NeuralNetwork:
     def __init__(self, genome: Genome, input: dict[int]):
         self.nodes: dict[int] = {}
         self.outputs = []
+        self.inputs = []
         for n in genome.nodes.genes:
             node = Node(n)
             node.type = n.type
@@ -27,9 +29,10 @@ class NeuralNetwork:
                 self.outputs.append(node)
             elif n.type == "INPUT":
                 node.ready = True
+                self.inputs.append(node)
         
         for i, value in input.items():
-            self.nodes[i].output = value
+            self.nodes[i].value = value
         
         for n, conn in genome.connections.genes.items():
             if not conn.Enabled:
@@ -37,12 +40,27 @@ class NeuralNetwork:
             _in = conn.Input
             _out = conn.Output
             _weight = conn.Weight
-            self.nodes[_out].input.append(_in)
-            self.weigth[_in] = _weight
+            self.nodes[_out].input.append(self.nodes[_in])
+            self.nodes[_in].output.append(self.nodes[_out])
+            self.nodes[_in].weight[self.nodes[_out]] = _weight
 
     def forward(self):
         # funcion iterativa que usa un stack
         stack = []
+        for n in self.inputs:
+            stack.append(n)
+        
+        while len(stack) != 0:
+            n = stack.pop()
+            if n.ready:
+                for i in n.output:
+                    i.value += n.value * n.weigth[i]
+                    i.input.remove(n)
+                    stack.append(i)
+            else:
+                if len(n.input) == 0:
+                    n.ready = True
+                stack.append(n)
 
     def train(self):        # Entrenar el modelo
         pass
