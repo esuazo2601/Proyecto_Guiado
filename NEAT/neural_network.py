ca99cd4c359d84a250944a746d1929e5a0c11412
Vicente Cser <vicentecser@gmail.com>
2023-07-19 21:08:57 -0400

    Add feed forward NN

--- NEAT/neural_network.py
+++ NEAT/neural_network.py
@@ -2,17 +2,47 @@ from .neat import NEAT
 from .genome import Genome
 from .connection_genes import ConnectionGenes, Connection
 from .node_genes import NodeGenes, Node
+from typing import Self
 
-import torch
-import torch.nn as nn
-
-from torch import autograd
 # Clase encargada de ejecutar la red seleccionada por neat y reconstruirla
 # a partir de los archivos .pickle generados por este
+
+class Node:
+    def __init__(self, type: str) -> None:
+        self.input: list[Self] = []
+        self.output: float
+        self.weight: dict[float]
+        self.type = type
+        self.ready = False
+
 class NeuralNetwork:
-    def __init__(self, genome: Genome):
-        # TODO: todo lo que es construir un modelo de pytorch
-        pass
+    def __init__(self, genome: Genome, input: dict[int]):
+        self.nodes: dict[int] = {}
+        self.outputs = []
+        for n in genome.nodes.genes:
+            node = Node(n)
+            node.type = n.type
+            self.nodes[n.id] = node
+            if n.type == "OUTPUT":
+                self.outputs.append(node)
+            elif n.type == "INPUT":
+                node.ready = True
+        
+        for i, value in input.items():
+            self.nodes[i].output = value
+        
+        for n, conn in genome.connections.genes.items():
+            if not conn.Enabled:
+                continue
+            _in = conn.Input
+            _out = conn.Output
+            _weight = conn.Weight
+            self.nodes[_out].input.append(_in)
+            self.weigth[_in] = _weight
+
+    def forward(self):
+        # funcion iterativa que usa un stack
+        stack = []
 
     def train(self):        # Entrenar el modelo
         pass
---
+++ test_gym.py
@@ -0,0 +1,12 @@
+import gymnasium
+env = gymnasium.make("ALE/Pacman-v5", obs_type="rgb", render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
