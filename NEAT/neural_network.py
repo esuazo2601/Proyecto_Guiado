9c8e714f45bd5339d4c150bba0df60c44dd750c1
Vicente Cser <vicentecser@gmail.com>
2023-07-20 09:54:16 -0400

    Fix nn constructor

--- NEAT/neural_network.py
+++ NEAT/neural_network.py
@@ -1,30 +1,39 @@
 from .neat import NEAT
 from .genome import Genome
 from .connection_genes import ConnectionGenes, Connection
-from .node_genes import NodeGenes, Node
+from .node_genes import NodeGenes, Genes
 from typing import Self
 
 # Clase encargada de ejecutar la red seleccionada por neat y reconstruirla
 # a partir de los archivos .pickle generados por este
 
-class Node:
+class Neuron:
     def __init__(self, type: str) -> None:
         self.input: list[Self] = []
-        self.output = list[Self] = []
+        self.output: list[Self] = []
         self.value: float = 0
-        self.weight: dict[Self]
+        self.weight: dict[Self] = {}
         self.type = type
         self.ready = False
+    
+    def __str__(self):
+        return f"""
+input = {self.input}
+output = {self.output}
+weigths = {self.weight}
+type = {self.type}
+ready = {self.ready}
+            """
 
 class NeuralNetwork:
     def __init__(self, genome: Genome, input: dict[int]):
-        self.nodes: dict[int] = {}
+        self.neuron: dict[int] = {}
         self.outputs = []
         self.inputs = []
         for n in genome.nodes.genes:
-            node = Node(n)
+            node = Neuron(n)
             node.type = n.type
-            self.nodes[n.id] = node
+            self.neuron[n.id] = node
             if n.type == "OUTPUT":
                 self.outputs.append(node)
             elif n.type == "INPUT":
@@ -32,7 +41,7 @@ class NeuralNetwork:
                 self.inputs.append(node)
         
         for i, value in input.items():
-            self.nodes[i].value = value
+            self.neuron[i].value = value
         
         for n, conn in genome.connections.genes.items():
             if not conn.Enabled:
@@ -40,10 +49,15 @@ class NeuralNetwork:
             _in = conn.Input
             _out = conn.Output
             _weight = conn.Weight
-            self.nodes[_out].input.append(self.nodes[_in])
-            self.nodes[_in].output.append(self.nodes[_out])
-            self.nodes[_in].weight[self.nodes[_out]] = _weight
+            self.neuron[_out].input.append(self.neuron[_in])
+            self.neuron[_in].output.append(self.neuron[_out])
+            self.neuron[_in].weight[self.neuron[_out]] = _weight
+        
+        for i, n in self.neuron.items():
+            print(i, ":", n)
 
+
+    # TODO: fix infinite loop
     def forward(self):
         # funcion iterativa que usa un stack
         stack = []
@@ -54,7 +68,7 @@ class NeuralNetwork:
             n = stack.pop()
             if n.ready:
                 for i in n.output:
-                    i.value += n.value * n.weigth[i]
+                    i.value += n.value * n.weight[i]
                     i.input.remove(n)
                     stack.append(i)
             else:
