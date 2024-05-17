1832817be5311f36d162e7f6218cf6edca2d8a3d
esuazo2601 <esteban.suazo.m2601@gmail.com>
2024-05-16 21:48:09 -0400

    fixed ?maybe?

--- NEAT/neural_network.py
+++ NEAT/neural_network.py
@@ -51,89 +51,74 @@ ready = {self.ready}
 
 class NeuralNetwork:
     def __init__(self, genome: Genome):
-        self.neuron: dict[int] = {}
-        self.outputs = []
-        self.inputs = []
-        for n in genome.nodes.genes:
-            node = Neuron(n)
-            node.type = n.type
-            self.neuron[n.id] = node
-            if n.type == "OUTPUT":
-                self.outputs.append(node)
-            elif n.type == "INPUT":
-                node.ready = True
-                self.inputs.append(node)
-        
-        #print(self.neuron)
-        for n, conn in genome.connections.genes.items():
-            if not conn.Enabled:
-                continue
-            _in = conn.Input
-            _out = conn.Output
-            _weight = conn.Weight
+            self.neuron: dict[int, Neuron] = {}
+            self.outputs = []
+            self.inputs = []
+
+            # Initialize neurons
+            for n in genome.nodes.genes:
+                node = Neuron(n.id)
+                node.type = n.type
+                self.neuron[n.id] = node
+                if n.type == "OUTPUT":
+                    self.outputs.append(node)
+                elif n.type == "INPUT":
+                    node.ready = True
+                    self.inputs.append(node)
             
-            # Check if input and output neurons exist
-            if _in in self.neuron and _out in self.neuron:
-                self.neuron[_out].input.append(self.neuron[_in])
-                self.neuron[_in].output.append(self.neuron[_out])
-                self.neuron[_in].weight[self.neuron[_out]] = _weight
-            else:
-                # If either input or output neuron is not found, pick any other neuron of the same type
-                if _in not in self.neuron:
-                    _in_type = genome.nodes.genes[_in].type
-                    for node in self.neuron.values():
-                        if node.type == _in_type:
-                            self.neuron[_out].input.append(node)
-                            node.output.append(self.neuron[_out])
-                            node.weight[self.neuron[_out]] = _weight
-                            break
-                if _out not in self.neuron:
-                    _out_type = genome.nodes.genes[_out].type
-                    for node in self.neuron.values():
-                        if node.type == _out_type:
-                            self.neuron[_out].input.append(node)
-                            node.output.append(self.neuron[_out])
-                            node.weight[self.neuron[_out]] = _weight
-                            break
+            # Print neuron information for debugging
+            print("Neurons initialized:", self.neuron.keys())
+
+            # Connect neurons based on genome connections
+            for conn in genome.connections.genes.values():
+                if not conn.Enabled:
+                    continue
+                _in = conn.Input
+                _out = conn.Output
+                _weight = conn.Weight
+
+                # Check if both input and output neurons exist
+                if _in in self.neuron and _out in self.neuron:
+                    self.neuron[_out].input.append(self.neuron[_in])
+                    self.neuron[_in].output.append(self.neuron[_out])
+                    self.neuron[_in].weight[self.neuron[_out]] = _weight
+                else:
+                    missing_neurons = []
+                    if _in not in self.neuron:
+                        missing_neurons.append(_in)
+                    if _out not in self.neuron:
+                        missing_neurons.append(_out)
+                    print(f"Warning: Missing neurons for connection: {_in} -> {_out}. Missing neuron IDs: {missing_neurons}")
+            
+            # DEBUG: Print out the connections for verification
+            #for neuron_id, neuron in self.neuron.items():
+            #    print(f"Neuron {neuron_id}: inputs: {[n.id for n in neuron.input]}, outputs: {[n.id for n in neuron.output]}")
         
-        # DEBUG
-        #for i, n in self.neuron.items():
-        #    print(i, ":", n)
 
 
     # TODO: fix infinite loop
-    def forward(self, _input: dict[int]):
-        # for i, value in _input.items():
-        #    self.neuron[i].value = value
+    def forward(self, _input: dict[int, float]):
         for node_id, value in _input.items():
             if node_id in self.neuron:
                 self.neuron[node_id].value = value 
-        #self.neuron = _input
-
+        
         queue = []
         for n in self.outputs:
             queue.append(n)
-            #print(queue)
-        while len(queue) != 0:
+        
+        while queue:
             n = queue.pop()
             n.ready = True
             for i in n.input:
                 if i.ready:
-                    n.value += i.value*i.weight[n]
+                    n.value += i.value * i.weight[n]
                 else:
                     n.ready = False
                     queue.insert(0, i)
             if n.ready:
                 if n.type != "INPUT":
-                    n.value = relu(n.value) #Aqui se cambia la funcion de activacion
-            else:
-                queue.insert(0, n)
+                    n.value = relu(n.value)  # Change activation function here if needed
         
-        #Se acumulan los valores de salida en un array
-        output_values = []
-        for x in self.outputs:
-            output_values.append(x.value)
-        
-        #return [x.value for x in self.outputs]
-        #Se retornan los valores normalizados con softmax
+        # Collect and return the output values, normalized with softmax
+        output_values = [x.value for x in self.outputs]
         return softmax(output_values)
