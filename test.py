324bc12c19242233c115e2c1250be8d815c3cb43
esuazo2601 <esteban.suazo.m2601@gmail.com>
2024-07-12 18:34:16 -0400

    fixed test file

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from NEAT.neat import NEAT
 import gymnasium as gym
 import numpy as np
+from NEAT.neural_network import NeuralNetwork
 
 model = NEAT(
         inputSize=128,
@@ -11,8 +12,9 @@ model = NEAT(
         C3=3.5,
 )
 
-model.load_genomes("save_ep_50")
+model.load_genomes("results_885.0")
 print(model.best_genome.fitness)
+net = NeuralNetwork(model.best_genome)
 
 env = gym.make("SpaceInvaders-ramDeterministic-v4",render_mode="human")
 
@@ -23,7 +25,7 @@ score = 0
 
 while not done and not truncated:
     state = {i: state[i] for i in range(len(state))}
-    actions = model.test(state)
+    actions = net.forward(state)
     final_action = np.argmax(actions)
     #print(final_action)
     n_state, reward, done, truncated, info = env.step(final_action)
